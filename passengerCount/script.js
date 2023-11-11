7a2241df6c9a141016a0a80bd1f6214e17774d3b
balkrishnray <balkrishnaroy31@gmail.com>
2023-11-11 10:36:36 +0545

    passenger count js

--- passengerCount/script.js
+++ passengerCount/script.js
@@ -1,13 +1,15 @@
-// document.getElementById("count").innerText = 5;
-// let myAge = 21;
-// let humanDogRatio = 7;
-// let myDogAge = myAge * humanDogRatio;
-// console.log(myDogAge);
 let count = 0;
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 function increment() {
-  count = count + 1;
-  let countel = document.getElementById("count-el");
-  countel.innerText = count;
-
+  count += 1;
+  countEl.textContent = count;
   console.log(count);
+}
+
+function save() {
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
 }
