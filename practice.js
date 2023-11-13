ba1d57298fce68ab5551a346b7a0cd93943850e0
balkrishnray <balkrishnaroy31@gmail.com>
2023-11-13 10:36:24 +0545

    practice

---
+++ practice.js
@@ -0,0 +1,27 @@
+// let firstName = "Balkrishna";
+// let lastName = "Ray";
+// let fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+// let fname = "Linda";
+// let greeting = "Hi there";
+// function welcome() {
+//   console.log(greeting + ", " + fname + "!");
+// }
+// welcome();
+
+let myPoints = 3;
+function add3Points(){
+    myPoints += 3;
+
+}
+function remove1Points(){
+    myPoints -= 1;
+}
+
+add3Points();
+add3Points();
+add3Points();
+remove1Points();
+remove1Points();
+console.log(myPoints);
