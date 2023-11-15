cf580041b00df8fdf1aece3669eb6726a0c51a2c
balkrishnray <balkrishnaroy31@gmail.com>
2023-11-15 21:25:14 +0545

    chrome extension

---
+++ chromeExtension/script.js
@@ -0,0 +1,14 @@
+let myLeads = ["www.google.com", "www.facebook.com", "www.twitter.com"];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+});
+
+for (let i = 0; i < myLeads.length; i++) {
+  // ulEl.innerHTML += "<li>" + myLeads[i] + " " + "</li>";
+  const li = document.createElement("li");
+  li.textContent = myLeads[i];
+  ulEl.append(li);
+}
