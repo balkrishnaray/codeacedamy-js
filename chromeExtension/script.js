98f123d4e093869af2409a7582293c2b9d49d6af
balkrishnray <balkrishnaroy31@gmail.com>
2023-12-02 15:36:11 +0545

    chrome extension for adding links

--- chromeExtension/script.js
+++ chromeExtension/script.js
@@ -1,14 +1,44 @@
-let myLeads = ["www.google.com", "www.facebook.com", "www.twitter.com"];
+let myLeads = [];
+let oldLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.getElementById("tab-btn");
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
+  inputEl.value = " ";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
 });
 
-for (let i = 0; i < myLeads.length; i++) {
-  // ulEl.innerHTML += "<li>" + myLeads[i] + " " + "</li>";
-  const li = document.createElement("li");
-  li.textContent = myLeads[i];
-  ulEl.append(li);
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+function render(Leads) {
+  let myList = " ";
+  for (let i = 0; i < Leads.length; i++) {
+    myList += `<li>
+      <a target =' _blank' href = '  ${Leads[i]} '> 
+       ${Leads[i]}  
+      </a> 
+      </li>`;
+  }
+  ulEl.innerHTML = myList;
 }
